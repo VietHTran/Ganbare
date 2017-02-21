fa92ed5455e983b6237ff14fc361a0dfb76e8d5a
Viet Hung Tran <trantechenterprise@gmail.com>
2017-02-20 23:58:18 -0500

    Add Server Class

--- Ganbare.cpp
+++ Ganbare.cpp
@@ -12,14 +12,13 @@
 #include <stdlib.h>
 #include <curl/curl.h>
 #include <thread>
+#include "server.h"
 
 using namespace std;
 using namespace cv;
 using namespace std::chrono;
 
-void detectAndDisplay( Mat frame, VideoCapture *capture_ptr );
-void playVideo(string file_name, Mat *frame_ptr,Mat *frame_gray_ptr,VideoCapture *capture_ptr);
-void checkAwake(Mat *frame_ptr,Mat *frame_gray_ptr, VideoCapture *capture_ptr);
+void detectAndDisplay( Mat frame);
 
 string face_cascade_name = "haarcascade_frontalface_alt.xml";
 string eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
@@ -28,10 +27,12 @@ CascadeClassifier eyes_cascade;
 String window_name = "Capture - Face detection";
 bool is_close=false, is_display=false;
 milliseconds close_time;
+Server server;
 
 int main( void )
 {
     ios::sync_with_stdio(false);
+    server.waitForClient();
     VideoCapture capture;
     Mat frame;
 
@@ -52,11 +53,12 @@ int main( void )
         }
 
         //-- 3. Apply the classifier to the frame
-        detectAndDisplay( frame, &capture );
+        detectAndDisplay( frame);
 
         char c = (char)waitKey(10);
         if( c == 27 ) { break; } // escape
     }
+    server.closeConnection();
     return 0;
 }
 
@@ -66,7 +68,7 @@ void resetCheck() {
 }
 
 /** @function detectAndDisplay */
-void detectAndDisplay( Mat frame, VideoCapture *capture_ptr )
+void detectAndDisplay( Mat frame)
 {
     std::vector<Rect> faces;
     Mat frame_gray;
@@ -101,57 +103,12 @@ void detectAndDisplay( Mat frame, VideoCapture *capture_ptr )
                 srand (time(NULL));
                 cout << "Target is sleeping" << endl;
                 is_display=true;
-                playVideo("test.mp4",&frame,&frame_gray, capture_ptr);
+
             }
         } else if (eyes.size()==0) {
             close_time= duration_cast< milliseconds >(system_clock::now().time_since_epoch());
             is_close=true;
         }
 
-    }
-}
-
-void playVideo(string file_name, Mat *frame_ptr,Mat *frame_gray_ptr,VideoCapture *capture_ptr) {
-    VideoCapture capture(file_name);
-    Mat frame;
-    string const WINDOW_NAME="vid";
-
-    if( !capture.isOpened() )
-        throw "Error openning video";
-
-    namedWindow( WINDOW_NAME, 0);
-    while (is_display) {
-        checkAwake(frame_ptr,frame_gray_ptr,capture_ptr);
-        capture >> frame;
-        if(frame.empty())
-            break;
-        imshow(WINDOW_NAME, frame);
-        waitKey(20);
-    }
-    destroyWindow(WINDOW_NAME);
-}
-
-void checkAwake(Mat *frame_ptr,Mat *frame_gray_ptr, VideoCapture *capture_ptr) {
-    //Transfer all minimum required instance using pointer to detect faces and eyes
-    Mat frame=*frame_ptr;
-    Mat frame_gray=*frame_gray_ptr;
-    VideoCapture capture=*capture_ptr;
-    std::vector<Rect> faces;
-    cout<<"1"<<endl;
-    capture.read(frame); cout<<"2"<<endl;
-    cvtColor( frame, frame_gray, COLOR_BGR2GRAY ); cout<<"3"<<endl;
-    equalizeHist( frame_gray, frame_gray ); cout<<"4"<<endl;
-
-    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) ); cout<<"5"<<endl;
-    if (faces.size()>1) {
-        resetCheck();
-    } else if (faces.size()==1) {
-        Mat faceROI = frame_gray( faces[0] );
-        std::vector<Rect> eyes;
-        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
-        if (eyes.size()>=2) {
-            resetCheck();
-            cout << "Stop the video" << endl;
-        }
     }
 }
