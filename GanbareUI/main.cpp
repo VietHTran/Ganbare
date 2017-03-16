17eb6d7b3bb3952d16fb23d77d64d476af85799c
Viet Hung Tran <trantechenterprise@gmail.com>
2017-03-16 00:01:54 -0400

    Use OpenCV scan in background thread

--- GanbareUI/main.cpp
+++ GanbareUI/main.cpp
@@ -1,24 +1,12 @@
 #include <QApplication>
-#include <opencv2/opencv.hpp>
-#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 
 #include <iostream>
-#include <unistd.h>
-#include <errno.h>
-#include <thread>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
 
 #include "mainwindow.h"
-#include "../headers/common.h"
 
 int main(int argc, char *argv[])
 {
-    ios_base::sync_with_stdio(false);
+    std::ios_base::sync_with_stdio(false);
 
     QApplication a(argc, argv);
     MainWindow w;
